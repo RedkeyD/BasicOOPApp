25ebfa6a8b1d9431bf2a299cf284318c927b20ff
Zievidinov <108923651+RedkeyD@users.noreply.github.com>
2023-02-12 17:56:36 +0300

    Add project files.

---
+++ BasicOOPApp/Form1.cs
@@ -0,0 +1,54 @@
+using MyCalc;
+
+namespace BasicOOPApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            double b;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToDouble(textBox3.Text);
+
+            MyCalc1 c = new MyCalc1(a, b);
+
+            textBox2.Text = Convert.ToString(c.findC());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BasicOOPApp/MyCalc1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalc
+{
+    public class MyCalc1
+    {
+        protected int a;
+        protected double b;
+
+        public MyCalc1()
+        {
+
+        }
+        public MyCalc1(int a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public double findC()
+        {
+            double c = (a + b) / (a - 1);
+            return c;
+        }
+    }
+
+    public class MyCalc2 : MyCalc1
+    {
+        protected double d;
+
+        public MyCalc2()
+        {
+        }
+
+        public MyCalc2(double d, double b, int a)
+        {
+            this.d = d;
+            this.b = b;
+            this.a = a;
+        }
+
+        public double findC2()
+        {
+            double c2;
+            if (d > a)
+            {
+                c2 = a + b - d;
+                return c2;
+            }
+            else
+            {
+                c2 = a - b;
+                return c2;
+            }
+        }
+    }
+    public class flyingObject 
+    {
+        private double length, speed;
+        private string name;
+        
+        public flyingObject() 
+        {
+            length = 0;
+            speed = 0;
+            name = " ";
+        }
+        public flyingObject(double length, double speed, string name) 
+        { 
+            this.length = length;
+            this.speed = speed;
+            this.name = name;
+        }
+
+        public double inOurUnit() 
+        {
+            double ourUnit = speed/ length;
+            return ourUnit;
+        }
+    }
+}
